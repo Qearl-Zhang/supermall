86a7f1cb038efa5b1b0b1979e60aa9efa798f1cf
caotyf <caotyf@yonyou.com>
2019-12-12 17:51:15 +0800

    修改批注

--- src/network/request.js
+++ src/network/request.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 export function request(config) {
   // 1.创建axios的实例
   const instance = axios.create({
-    baseURL: 'http://106.54.54.237:8000/api/v1',
+    baseURL: 'http://106.54.54.237:8000/api/v1',  //注意：why的接口地址变更
     timeout: 5000
   })
   // 2.axios的拦截器
